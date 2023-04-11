82b5b9a2bcc185208b13680536218edefbf683a4
Gauthier <gauthier.mauche@gmail.com>
2023-04-11 22:07:07 +0200

    add route for create new match

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,7 @@
 <nav>
 	<header class="bg-gray-800 text-white flex justify-between items-center px-4 py-3">
 		<div class="flex items-center">
-			<a href="#" class="font-bold text-xl">Tictactoe</a>
+			<a href="/" class="font-bold text-xl">Tictactoe</a>
 		</div>
 		<div class="flex items-center">
 			{#if !$page.data.user}
@@ -21,6 +21,7 @@
 			{/if}
 
 			{#if $page.data.user}
+				<a href="/match/new">Créer une partie</a>
 				<form
 					class="logout mx-3 hover:text-gray-400"
 					action="/logout"
