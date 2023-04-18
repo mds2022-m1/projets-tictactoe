836f758b1da4c9d61879299aed2a73ce513f25b5
Gauthier <gauthier.mauche@gmail.com>
2023-04-18 11:17:09 +0200

    update start match and add player

--- src/routes/(protected)/match/[id]/+page.server.ts
+++ src/routes/(protected)/match/[id]/+page.server.ts
@@ -10,33 +10,30 @@ interface CustomLocals extends Record<string, any> {
 }
 
 export const load: PageServerLoad = async ({ params: { id }, locals }: { params: { id: string }, locals: CustomLocals }) => {
+    const { user } = locals as CustomLocals;
 
-	    const { user } = locals as CustomLocals;
+    const match = await getMatchAndUserMatchById(id);
+    const userIds = match?.user_matches?.map(({ user_id }) => user_id) ?? [];
 
+    if (!user?.id || (!userIds?.includes(user.id) && match?.user_matches.length === 2)) {
+        throw redirect(303, '/');
+    }
 
-		let match = await getMatchAndUserMatchById(id);
+    if (!userIds?.includes(user.id)) {
+        const isCreator = !userIds.length;
+        await Promise.all([
+            createUserMatch(id, user.id, isCreator),
+            match?.user_matches.length === 2 && createUserMatch(id, user.id, !isCreator)
+        ]);
+    }
+	
+    if (!match) {
+        throw redirect(303, '/');
+    }
 
-		const userIds = match?.user_matches.map((userMatch) => userMatch.user_id);
-
-		if (!user?.id || (!userIds?.includes(user.id) && match?.user_matches.length === 2)) {
-			throw redirect(303, '/');
-		}
-
-		if (!userIds?.includes(user.id)) {
-			const userMatch = await createUserMatch(id, user.id);
-		}
-
-		match = await getMatchAndUserMatchById(params.id);
-
-		if (!match) {
-			return {
-				success: false,
-			};
-		}
-		return {
-			success: true,
-			match,
-			userId: user.id,
-		};
-	throw redirect(303, '/');
+    return {
+        success: true,
+        match,
+        userId: user.id
+    };
 };
