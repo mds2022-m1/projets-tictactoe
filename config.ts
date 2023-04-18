3bc4637cff113fba047b8657130e2b843877f65c
Gauthier <gauthier.mauche@gmail.com>
2023-04-18 11:22:46 +0200

    add socket for play and update config.ts

--- config.ts
+++ config.ts
@@ -7,54 +7,6 @@ export default function configServerWebsocket(server: HttpServer) {
 	// init socket server
 	const io = new Server(server);
 
-	// connection event
-	// io.on('connect', (socket) => {
-
-	//     // console.log("new client connected");
-
-	//     socket.on('test', (data) => {
-	//         console.log(data);
-	//     });
-
-	//     socket.on('playerJoined', (matchData) => {
-
-	//         socket.join(matchData.match_id);
-	//         // log room members
-	//         // console.log(io.sockets.adapter.rooms);
-
-	//         // // io.emit('matchUpdate', {
-	//         // //     match: match,
-	//         // //     action: 'create',
-	//         // //     idUser: socket.id
-	//         // // });
-	//         // // io emit number of users in room
-	//         let users = io.sockets.adapter.rooms.get(matchData.match_id);
-
-	//         // io.to(match.id).emit('matchUpdate', {
-	//         //     match: match,
-	//         //     action: 'create',
-	//         //     idUser: socket.id,
-	//         //     users: users?.size
-	//         // });
-
-	//         io.to(matchData.match_id).emit('playerJoined', {
-	//             match_id: matchData.match_id,
-	//             user_id: matchData.user_id,
-	//             users: users?.size
-	//         });
-
-	//     });
-
-	//     socket.on('playerMoved', (playerMoved) => {
-	//         io.emit("playerMoved", playerMoved);
-	//     });
-
-	//     socket.on('disconnect', () => {
-	//         console.log('user disconnected');
-	//     });
-
-	// });
-
 	io.use(async (socket, next) => {
 		const sessionID = socket.handshake.auth.sessionID;
 
@@ -83,47 +35,9 @@ export default function configServerWebsocket(server: HttpServer) {
 			userID: socket.userID,
 		});
 
-		//     // console.log("new client connected");
+		socket.on('test', (data) => {
+			console.log(data);
+		});
 
-		//     socket.on('test', (data) => {
-		//         console.log(data);
-		//     });
-
-		//     socket.on('playerJoined', (matchData) => {
-
-		//         socket.join(matchData.match_id);
-		//         // log room members
-		//         // console.log(io.sockets.adapter.rooms);
-
-		//         // // io.emit('matchUpdate', {
-		//         // //     match: match,
-		//         // //     action: 'create',
-		//         // //     idUser: socket.id
-		//         // // });
-		//         // // io emit number of users in room
-		//         let users = io.sockets.adapter.rooms.get(matchData.match_id);
-
-		//         // io.to(match.id).emit('matchUpdate', {
-		//         //     match: match,
-		//         //     action: 'create',
-		//         //     idUser: socket.id,
-		//         //     users: users?.size
-		//         // });
-
-		//         io.to(matchData.match_id).emit('playerJoined', {
-		//             match_id: matchData.match_id,
-		//             user_id: matchData.user_id,
-		//             users: users?.size
-		//         });
-
-		//     });
-
-		//     socket.on('playerMoved', (playerMoved) => {
-		//         io.emit("playerMoved", playerMoved);
-		//     });
-
-		//     socket.on('disconnect', () => {
-		//         console.log('user disconnected');
-		//     });
 	});
 }
--- src/routes/(protected)/match/[id]/+page.svelte
+++ src/routes/(protected)/match/[id]/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import type { Socket } from 'socket.io-client';
+	import { getContext } from 'svelte';
 
+	const socket: Socket = getContext('socket') as Socket;
 
 	export let data: PageData;
 	
 	const { user, match } = data;
 	
+	socket.emit('test', 'test');
 	
 </script>
-
-{JSON.stringify(data)}
